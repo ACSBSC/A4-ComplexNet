2336b74c39a3599e2143cb725fcef8a48e6d206c
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-04-26 14:55:59 +0200

    settings

--- SIS.py
+++ SIS.py
@@ -1,3 +1,24 @@
+import random as rnd
 
-def SIS():
-    print()
+def SIS(G):
+    S = "S"                 #Healthy
+    I = "I"                 #Infected
+    recovery = 0.5          #probability of getting cured
+    infection = 0.5         #probability of getting infected
+    list_fraq_infected = [] #average fraction of infected node per cycle
+    time_step = 1000
+    
+    for i in range(time_step):
+        for node in G.nodes:
+            if node =="I":
+                rn = rnd.random()
+                if rn <recovery:
+                    G.node=S
+            else:
+              print()  
+    
+    
+    #avrg_fraq_infected = sum(list_fraq_infected)/len(list_fraq_infected) #average of averages fraction of infected node
+    
+    
+    
--- monte-carlo.py
+++ monte-carlo.py
@@ -1,3 +1,7 @@
 
-def montecarlo():
-    print()
+def montecarlo(G, reptetition):
+    infection_probability = 0.2
+    time_steps = 1000
+    transitory_steps = 900
+    stationary_steps = time_steps-transitory_steps
+    
