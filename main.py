707b67ad8184a15c34e36203ecd45a8541277ecf
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-04-26 15:43:42 +0200

    update on main

--- main.py
+++ main.py
@@ -6,12 +6,5 @@ warnings.filterwarnings("ignore")
 
 n=10
 p = 0.1
-
 G = nx.erdos_renyi_graph(n, p)
 
-print(G.nodes)
-
-for i in G.nodes.keys():
-    G.nodes[i]['state'] = 'S'
-    
-print(G.nodes(data=True))
